3c9bfa3b968a2032d268091ba726cee3774f144c
rPawel <p.ruchaj@yahoo.co.uk>
2016-02-13 19:18:20 +0000

    reordered backup with pruning

--- inputplugins/datavolume.py
+++ inputplugins/datavolume.py
@@ -6,8 +6,9 @@ remove_older_than = "10D"
 
 def backup(app_path, dst_path):
     app_data_path = app_path + "/data"
-    remove_old_cmd = rdiff_backup + " --verbosity 1 --remove-older-than " + remove_older_than + " --force " + dst_path
-    shell.retry_run([remove_old_cmd], 3)
     backup_cmd = rdiff_backup + " --verbosity 1 --exclude-device-files --exclude-fifos --exclude-sockets " \
                                 "--exclude-if-present .backupignore " + app_data_path + " " + dst_path
     shell.retry_run([backup_cmd], 3)
+
+    remove_old_cmd = rdiff_backup + " --verbosity 1 --remove-older-than " + remove_older_than + " --force " + dst_path
+    shell.retry_run([remove_old_cmd], 3)
